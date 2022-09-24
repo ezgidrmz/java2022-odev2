ab75f99b190d55a9c25ff959887e9535b61f7fd4
ezgidrmz <75444757+ezgidrmz@users.noreply.github.com>
2022-09-24 16:55:04 +0300

    Add files via upload

---
+++ methods/src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        sayiBulmaca();
+    }
+    public static void sayiBulmaca(){
+        int[] sayilar=new int[]{1,2,5,7,9,0};
+        int aranacak=6;
+        boolean varMi=false;
+
+        for (int sayi: sayilar) {
+            if (sayi==aranacak){
+                varMi =true;
+                break;
+            }
+        }
+        String mesaj="";
+        if(varMi){
+            mesaj="Sayı mevcuttur:"+aranacak;
+            mesajVer(mesaj);
+                }else {
+            mesajVer("Sayı mevcut değildir:"+aranacak);
+        }
+    }
+    public static void mesajVer(String aranacak){
+        System.out.println("Sayı mevcuttur:"+aranacak);
+    }
+}
